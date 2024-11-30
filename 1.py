60dcc5f471639c662691925ac1d50d2203e3e053
email <ckddlf050@naver.com>
2024-11-30 13:52:04 +0900

    test

--- 1.py
+++ 1.py
@@ -71,8 +71,17 @@
 # print(str(2)*10)
 # print(str(2)+"입니다")
 
-print('|\_/|')
-print('|q p|   /}')
-print('( 0 )"""\\')
-print('|"^"\'   |')
-print('||_/=\\__|')
+# print('|\_/|')
+# print('|q p|   /}')
+# print('( 0 )"""\\')
+# print('|"^"\'   |')
+# print('||_/=\\\__|')
+
+name = input("이름을 입력하세요")
+age = input("나이를 입력하세요")
+print(f"안녕하세요! {name}님 ({age}세)")
+
+name = input("이름을 입력하세요")
+year = int(input("태어난 년도를 입력하세요"))
+year1 = int(input("올해 년도를 입력하세요"))
+print(f"올해는  {year1} 년, {name} 님의 나이는 {year1 - year +1}세 입니다")
