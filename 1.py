0051978ca3926b94157e05a3e44ad331943bb99c
email <ckddlf050@naver.com>
2024-11-30 11:20:33 +0900

    test2

--- 1.py
+++ 1.py
@@ -4,8 +4,75 @@
 # print("코딩온입니다.")
 
 
-ive = "럭키비키"
-print(ive)
-ive = "장원영"
-print(ive)
+# ive = "럭키비키"
+# print(ive)
+# ive = "장원영"
+# print(ive)
 
+# print(f"나는 {ive}입니다.")
+# print("나는 " , ive ,"입니다.",sep="")
+# print("나는 " + ive +"입니다.")
+
+
+# print(type(77),type(7.2))
+# print(type(7.2))
+# print(type("a"))
+
+# print("what\"s your name")
+# print("1111\t111")
+
+# num = 10
+# b_num = 0b1010
+# h_num = 0xA
+
+# print(num)
+# print(b_num)
+# print(h_num)
+
+# print(bin(10))
+# print(hex(10))
+
+# print(ord('0'))
+# print(ord('A'))
+# print(chr(48))
+# print(chr(65))
+
+# print(3//2)
+# print(3.25//2)
+# print(11%3)
+# print(3.25%7)
+# print(2**3)
+# print(2**10)
+
+# print(1+2*3**2)
+# print(1+(2*(3**2)))
+# print(1+2*-3**2)
+# print(1+2*(-3)**2)
+
+# print(f"빵의 개수 : ", 30//4)
+# print(f"남은 빵의 개수 : ", 30%4)
+
+# a= 1
+# a +=1 #a = a+1
+# print(a)
+
+# print("안녕 " + "반가워")
+# print("방가 " * 10)
+
+# song = input("요즘 자주 듣는 노래는?")
+# print(song)
+# print(type(song))
+
+# a=input("1+1=?")
+# print(a)
+# print(int(a)*2)
+
+# a = 2
+# print(str(2)*10)
+# print(str(2)+"입니다")
+
+print('|\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"\'   |')
+print('||_/=\\__|')
